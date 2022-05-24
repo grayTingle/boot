0b3cfa98f86dfff3d90ed3eb3d7d64bb5bbdb648
grayTingle <atingle@unica.edu.ni>
2022-05-24 17:56:06 -0600

    exam-exam

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var color = $(".selected").css("background-color");
 var $canvas = document.getElementById("myCanvas");
-var context = $canvas[0].getContext("2d");
+var context = $canvas.getContext("2d");
 var lastEvent;
 var mouseDown = false;
 
-//When clicking on control list items
+//when clicking on control list items
 $(".controls").on("click", "li", function() {
-  //Deselect sibling elements
+  //deselect sibling elements
   $(this).siblings().removeClass("selected");
-  //Select clicked element
+  //select clicked element
   $(this).addClass("selected");
-  //cache current color
+  //cache the current color
   color = $(this).css("background-color");
 });
 
-//When "New Color" is pressed
+//when you hit new color
 $("#revealColorSelect").click(function() {
-  //Show color select or hide the color select
+  //color select show or hide
   changeColor();
   $("#colorSelect").toggle();
 });
@@ -29,35 +29,40 @@ function changeColor() {
   $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
 }
 
-//When color sliders change
+//when color sliders change
 $("input[type=range]").change(changeColor);
 
-//When "Add Color" is pressed
+//when you hit add color
 $("#addNewColor").click(function() {
-  //Append the color to the controls ul
+  //put it in the ul
   var $newColor = $("<li></li>");
   $newColor.css("background-color", $("#newColor").css("background-color"));
   $(".controls ul").append($newColor);
-  //Select the new color
+  //select the new color
   $newColor.click();
 });
 
-//On mouse events on the canvas
-$canvas.mousedown(function(e) {
+//when you  do a draw and hold down your mouse
+$canvas.onmousedown=function(e) {
   lastEvent = e;
   mouseDown = true;
-}).mousemove(function(e) {
-  //Draw lines
+}
+$canvas.onmousemove=function(e) {
+
+  //please actually draw something
   if (mouseDown) {
     context.beginPath();
     context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
     context.lineTo(e.offsetX, e.offsetY);
+    context.lineWidth = 4;
     context.strokeStyle = color;
     context.stroke();
     lastEvent = e;
   }
-}).mouseup(function() {
+}
+$canvas.onmouseup=function() {
   mouseDown = false;
-}).mouseleave(function() {
-  $canvas.mouseup();
-});
+}
+$canvas.onmouseleave=function() {
+  $canvas.onmouseup();
+}
